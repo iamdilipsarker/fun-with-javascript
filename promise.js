556505bc5c8bf33a13c00fc1dd59a6f58aca1d7a
Dilip Sarker <dilipsarker719@gmail.com>
2023-10-28 15:48:59 +0600

    small changes

--- promise.js
+++ promise.js
@@ -2,7 +2,7 @@ const herResponse = new Promise((resolve, reject) => {
 	reject("You don't deserve me");
 });
 
-const OurRequest = herResponse
+const myRequest = herResponse
 	.then((resolveResponse) => {
 		console.log(resolveResponse);
 	})
